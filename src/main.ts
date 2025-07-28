1216df22b991c153d1cc8e45dca2b70282b6a7db
maruchanazalia <maruchanycamaron26@gmail.com>
2025-07-28 08:00:15 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,16 @@ import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: [
+      'https://dasboardadmincl.netlify.app/',
+      'http://localhost:5173',
+      'https://tusitio.netlify.app'
+    ],
+    credentials: true,
+  });
+
   await app.listen(envs.port ?? 3000);
 }
 bootstrap();
